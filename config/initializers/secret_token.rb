33063ce3604050ef382e3feb016bb52f3c0f3c38
mspiegs <matt@mycaseinc.com>
2014-07-16 15:47:14 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prodcase::Application.config.secret_key_base = '2358b7943d77d84348e09102d17855729c5f05f00ca1db5b73f847dbfc37cebd7e0cdfd8de56ee911d590fb52c01135215699b031d8ca1c8ed73febe1b427ccb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prodcase::Application.config.session_store :cookie_store, key: '_prodcase_session'
